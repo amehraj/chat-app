18b6bb3f6dfa95e1ac2bcdf4ee2408b54bea6169
Ali Mehraj <ali.mehraj@tuni.fi>
2022-01-02 19:23:24 +0200

    Initial setup with socket.io

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 const path = require('path');
 const express = require('express');
+const http = require('http');
+const socketio = require('socket.io');
+
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
 const port = process.env.PORT || 3000;
 const publicDirPath = path.join(__dirname, '../public/');
 
 app.use(express.static(publicDirPath));
 
-app.listen(port, () => {
+io.on('connection', () => {
+    console.log('New user connected');
+})
+
+server.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
