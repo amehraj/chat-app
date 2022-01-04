4460193e3ad3f4f4a0534dfa252db3190a571fc5
Ali Mehraj <ali.mehraj@tuni.fi>
2022-01-04 17:03:42 +0200

    Setup Socket.io for chat and sharing location

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,47 @@
-io();
+const socket = io();
+
+const $messageForm = document.querySelector('#message-form');
+const $messageFormInput = $messageForm.querySelector('input');
+const $messageFormButton = $messageForm.querySelector('button');
+const $shareLocationButton = document.querySelector('#share-location');
+
+socket.on('message', (message) => {
+    console.log(message);
+});
+
+$messageForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    $messageFormButton.setAttribute('disabled','disabled');
+
+    
+    const message = event.target.elements.message.value;
+    socket.emit('sendMessage', message, (error) => {
+
+        $messageFormButton.removeAttribute('disabled');
+        $messageFormInput.value = "";
+        $messageFormInput.focus();
+
+        if(error){
+            return console.log('Someting went wrong while delivering the message');
+        }
+        console.log('Message Delivered!');
+
+    });
+});
+
+$shareLocationButton.addEventListener('click', () => {
+    if(!navigator.geolocation){
+        alert('Your browser does not support geolocation');
+    }
+    $shareLocationButton.setAttribute('disabled','disabled');
+
+    navigator.geolocation.getCurrentPosition((position) => {
+        socket.emit('shareLocation', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        }, () => {
+            $shareLocationButton.removeAttribute('disabled');
+            console.log('Location Shared!');
+        });
+    });
+});
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 const http = require('http');
 const socketio = require('socket.io');
+const Filter = require('bad-words');
 
 
 const app = express();
@@ -13,9 +14,30 @@ const publicDirPath = path.join(__dirname, '../public/');
 
 app.use(express.static(publicDirPath));
 
-io.on('connection', () => {
+io.on('connection', (socket) => {
     console.log('New user connected');
-})
+    socket.emit('message', 'Welcome!');
+
+    socket.broadcast.emit('message', 'A new user has joined!');
+
+    const filter = new Filter();
+
+    socket.on('sendMessage', (message, callback) => {
+        message = filter.clean(message);
+        io.emit('message', message);
+
+        callback();
+    }); 
+
+    socket.on('shareLocation', (coords, callback) => {
+        io.emit('message', `https://www.google.com/maps?q=${coords.latitude},${coords.longitude}`);
+        callback();
+    });
+
+    socket.on('disconnect', () => {
+        io.emit('message', "A user has disconnected!");
+    });
+});
 
 server.listen(port, () => {
     console.log(`Server running on port ${port}`);
